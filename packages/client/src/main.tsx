237024a78a1dad6810905b0fa93696ff6b6bd645
ta-nett@mail.ru <klmnopgg>
2022-11-13 19:22:36 +0300

    fix BrRouter import

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-import { BrowserRouter as BRouter} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BRouter >
+    <BrowserRouter >
       <App />
-    </BRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
