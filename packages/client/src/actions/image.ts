98c866d3f2aca1d8954d31db26b127256f04c724
octo <v.minaev@qiwi.com>
2022-11-18 12:10:54 +0300

    typescript

--- packages/client/src/actions/image.ts
+++ packages/client/src/actions/image.ts
@@ -1,11 +1,18 @@
+import { typePoint, typeImage } from "../store";
+
 export enum ImageActions {
   AddPoint = 'addPoint',
   AddSegment = 'addSegment',
   RemoveSegment = 'removeSegment',
   SetImage = 'setImage',
 }
+export type typeImageAction = {
+  type: ImageActions
+  point?: typePoint
+  image?: typeImage
+}
 
-export const addPoint = (point) => ({ type: ImageActions.AddPoint, point });
-export const addSegment = (point) => ({ type: ImageActions.AddSegment, point });
+export const addPoint = (point: typePoint) => ({ type: ImageActions.AddPoint, point });
+export const addSegment = (point: typePoint) => ({ type: ImageActions.AddSegment, point });
 export const removeSegment = () => ({ type: ImageActions.RemoveSegment });
-export const setImage = (image) => ({ type: ImageActions.SetImage, image });
+export const setImage = (image: typeImage) => ({ type: ImageActions.SetImage, image });
--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -2,4 +2,15 @@ import { createStore } from 'redux';
 
 import { reducers } from '../reducers'
 
+export type typePoint = {
+  x: number
+  y: number
+}
+export type typeSegment = Array<typePoint>;
+export type typeImage = Array<typeSegment>;
+
+export type typeState = {
+  image: typeImage
+}
+
 export const store = createStore(reducers);
