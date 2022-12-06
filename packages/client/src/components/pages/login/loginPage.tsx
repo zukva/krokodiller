bc12f1eea748b1724446bd926afaf8c1ccbe15ac
octo <v.minaev@qiwi.com>
2022-12-06 13:47:19 +0300

    formik the bigining

--- packages/client/src/components/pages/login/loginPage.tsx
+++ packages/client/src/components/pages/login/loginPage.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { Formik, Form } from 'formik'
+
+import { ROUTS } from '../../../routs/routsList'
+import { getFields, typeFormConfig } from '../../../helpers/getFields'
+
+const FIELDS: typeFormConfig = {
+  login: { label: 'логин', value: '' },
+  password: { label: 'пароль', value: '' },
+}
 
 export const LoginPage: React.FC = () => {
-  return(
+  const isAuth = false // TODO: тут брать из редукса
+  const navigate = useNavigate()
+
+  const redirect = () => {
+    navigate(ROUTS.REGISTER_PAGE)
+  }
+
+  const { initialValues, fields } = getFields(FIELDS)
+
+  return (
     <div>
-      login page
+      {isAuth && <Navigate to={ROUTS.PROFILE_PAGE}></Navigate>}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={values => {
+          console.log(values)
+        }}>
+        <Form>
+          {fields}
+          <button type="submit">войти</button>
+        </Form>
+      </Formik>
+      <button onClick={redirect}>зарегестрироваться</button>
     </div>
   )
 }
--- packages/client/src/routs/MainRouter.tsx
+++ packages/client/src/routs/MainRouter.tsx
@@ -11,35 +11,51 @@ import { ProfilePage } from '../components/pages/profile/profile'
 import { GamePage } from '../components/pages/gamePage/gamePage'
 import ProtectedRoute from '../HOC/ProtectedRout'
 
-
 interface IMainRouter {
   isAuth: boolean
 }
 
-export const MainRouter: React.FC<IMainRouter> = ({isAuth}) => {
-
-
-
+export const MainRouter: React.FC<IMainRouter> = ({ isAuth }) => {
   return (
-
     <ErrorBoundary>
       <Routes>
+        <Route path={ROUTS.LOGIN_PAGE} element={<LoginPage />} />
+        <Route path={ROUTS.REGISTER_PAGE} element={<RegisterPage />} />
+        <Route
+          path={ROUTS.LEADERBOARD_PAGE}
+          element={
+            <ProtectedRoute isAuth={isAuth}>
+              <LeaderBoardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={ROUTS.FORUM_PAGE}
+          element={
+            <ProtectedRoute isAuth={isAuth}>
+              <ForumPage />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path={ ROUTS.LOGIN_PAGE } element={ <LoginPage /> } />
-        <Route path={ ROUTS.REGISTER_PAGE } element={ <RegisterPage /> } />
-        <Route path={ ROUTS.LEADERBOARD_PAGE }
-               element={ <ProtectedRoute isAuth={ isAuth }><LeaderBoardPage /></ProtectedRoute> } />
-        <Route path={ ROUTS.FORUM_PAGE } element={ <ProtectedRoute isAuth={ isAuth }><ForumPage /></ProtectedRoute> } />
-
-        <Route path={ ROUTS.PROFILE_PAGE } element={
-          <ProtectedRoute isAuth={ isAuth }>
-            <ProfilePage />
-          </ProtectedRoute> } />
-        <Route path={ ROUTS.GAME_PAGE } element={ <ProtectedRoute isAuth={ isAuth }><GamePage /></ProtectedRoute> } />
-        <Route path={ ROUTS.START_PAGE } index element={ <StartPage /> }></Route>
-
+        <Route
+          path={ROUTS.PROFILE_PAGE}
+          element={
+            <ProtectedRoute isAuth={isAuth}>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={ROUTS.GAME_PAGE}
+          element={
+            <ProtectedRoute isAuth={isAuth}>
+              <GamePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path={ROUTS.START_PAGE} index element={<StartPage />}></Route>
       </Routes>
     </ErrorBoundary>
-
   )
 }
